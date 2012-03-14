024c4a0afb2c2467e68d478b99abebea929088a6
Marcus Tyren <marcus.tyren@gmail.com>
2012-03-14 12:55:24 +0100

    lade till anton

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 	public static void main(String[] arg) {
 		System.out.println("Hello World!");
 		System.out.println("Spring Zlatan!");
+		System.out.println("Run Anton!Run like the wind!");
 	}
 
 }
