d42b340e1838822f8ce5481fd17fce90ffeb500c
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-03-15 17:52:51 +0100

    testKl1752

--- src/nyTest.java
+++ src/nyTest.java
@@ -2,5 +2,6 @@
 public class nyTest {
 	public static void main(String []args){
 		System.out.println("yes testen funkar!!!");
+		System.out.println("Hur faen kan man göra nånting så jävla dumt så det blir en låsning här?");
 	}
 }
