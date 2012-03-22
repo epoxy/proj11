a0759d98a11051c1969da140df1afb7e23d9dd70
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-03-22 13:44:57 +0100

    added Player-methods

--- src/se/chalmers/projectBomberMan/Player.java
+++ src/se/chalmers/projectBomberMan/Player.java
@@ -1,5 +1,24 @@
 package se.chalmers.projectBomberMan;
 
 public class Player {
-
+	int oldX;
+	int oldY;
+	int newX;
+	int newY;
+	public void movePlayerDir(int direction){
+		int deltaX;
+		int deltaY;
+		if(direction==1){
+			deltaX =1;
+			deltaY=0;
+		}
+		
+		GameTile t = gameBoard.getTile(x+deltaX, y+deltaY);
+				t.receivesPlayer(this);
+	}
+	public void movePlayer(int x, int y){
+		newX=x;
+		newY=y;
+		//player.getTile(newX,newY);
+	}
 }
